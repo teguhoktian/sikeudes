7e92584d1f7922da139bf605b7138b8586899858
Hoki Teguh Oktian <42082482+teguhoktian@users.noreply.github.com>
2021-11-14 10:53:33 +0700

    Hilangkan Route Register

--- routes/web.php
+++ routes/web.php
@@ -49,7 +49,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
 
-Auth::routes();
+Auth::routes(['register' => false]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
